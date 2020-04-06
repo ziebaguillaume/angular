2b294cc367b15cc672a70cb961aec83bde3b5e06
gz <guillaume.zieba@laposte.net>
2020-04-06 17:05:01 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  SelectedItem: any = { 
+  title: "Corobabavirus",
+  img: "../assets/corobaba.jpg",
+  description: "Baba virus",
+};
+
+   liste : any[]= 
+  [
+ {
+   title: "Corobabavirus",
+   img: "../assets/corobaba.jpg",
+   description: "Baba virus",
+   id:0,
+ },
+ {
+   title: "Coronessvirus",
+   img: "../assets/baba.jpg",
+   description: "Couille virus",
+   id:1,
+ },
+ {
+   title: "Corinavirus",
+   img: "../assets/daronnehenry.jpg",
+   description: "La Daronne de Henry",
+   id:2,
+ }
+]
+
+UpdateMain(id){
+
+  this.SelectedItem=this.liste[id];
+}
+
 }
